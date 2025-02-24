2cdd207cf0cff37b2d99ea2ea9f5681c45e992cc
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-02-25 04:02:12 +0530

    mongodb conneevted

--- job-tracker/Backend/app.js
+++ job-tracker/Backend/app.js
@@ -3,15 +3,19 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+// Load environment variables
 dotenv.config();
-connectDB(); // Connect MongoDB
 
-const app  = express()
+// Connect MongoDB
+connectDB();
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("Job Tracker API is running...");})
+app.get("/", (req, res) => {
+  res.send("Job Tracker API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server  is running  on port 5000 ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
